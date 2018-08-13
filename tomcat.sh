3d1815ce332b6fc108b5fc9eeaa9db49aecbf74d
Ratiborec <mikhailznak@gmail.com>
2018-08-13 17:36:36 +0300

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#cd /usr/local/src/ && wget  --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+#"http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+#yum localinstall jdk-8u*-linux-x64.rpm -y
+#rm -f /usr/local/src/jdk-8u*-linux-x64.rpm
+
 yum install java java-devel net-tools vim -y
 
 
